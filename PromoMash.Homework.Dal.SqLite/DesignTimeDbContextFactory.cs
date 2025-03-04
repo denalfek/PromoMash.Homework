8ec5ea24511e1b38b38f0489e5196dda97b034ba
Denis Sokolov <sokolovdd@ddplanet.ru>
2025-03-04 23:05:30 +0300

    db context fine tune

---
+++ PromoMash.Homework.Dal.SqLite/DesignTimeDbContextFactory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PromoMash.Homework.Dal.SqLite;
+
+internal sealed class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<PromoMashDbContext>
+{
+    public PromoMashDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<PromoMashDbContext>();
+        return new PromoMashDbContext(optionsBuilder.Options);
+    }
+}
--- PromoMash.Homework.Dal.SqLite/PromoMashDbContext.cs
+++ PromoMash.Homework.Dal.SqLite/PromoMashDbContext.cs
@@ -6,4 +6,21 @@ namespace PromoMash.Homework.Dal.SqLite;
 internal sealed class PromoMashDbContext(DbContextOptions<PromoMashDbContext> opts) : DbContext(opts)
 {
     public DbSet<User> Users { get; set; }
+
+    private const string ConnectionString = "Data Source=app.db";
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<User>()
+            .HasKey(x => x.Id);
+
+        modelBuilder
+            .Entity<User>()
+            .HasIndex(u => u.Email)
+            .IsUnique();            
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite(ConnectionString);
 }
