96932dba508e43c177aa238ef38c9c3f47617afc
Denis Sokolov <sokolovdd@ddplanet.ru>
2025-03-09 10:29:08 +0700

    transfer weather forecast to the external component; clean app component

--- PromoMash.Homework.Web/promomash.homework.web.client/src/app/app.module.ts
+++ PromoMash.Homework.Web/promomash.homework.web.client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherForecastComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
